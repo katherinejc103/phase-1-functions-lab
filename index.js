77b0f99c7f8798a9ff78adeb3b587506f70aac10
Katherine Jean-Charles <katherinejc103@gmail.com>
2021-12-27 14:23:38 -0500

    Done

--- index.js
+++ index.js
@@ -15,20 +15,17 @@ function distanceTravelledInFeet (start, destination) {
 
 //part four
 function calculatesFarePrice (start, destination) {
-    return ((Math.abs(destination - start) * 264) - 400);
-}
-    if (calculatesFarePrice <= 400) {
-    console.log('gives customer a free sample')
-} else if (calculatesFarePrice > 400 && calculatesFarePrice <= 2000) {
-    console.log((calculatesFarePrice) * .2)
-} else if (calculatesFarePrice > 2000) {
-    console.log((calculatesFarePrice) + 25)
-} else {
-    calculatesFarePrice > 2500;
-    console.log('cannot travel that far');
-}
-
-
+    //return ((Math.abs(destination - start) * 264))
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return ((distance - 400) * .02);
+    } else if (distance > 2000 && distance < 2500) {
+      return 25;   
+    } else { 
+       return 'cannot travel that far';
+    }}
 
 
 
